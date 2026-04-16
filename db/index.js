d18efaa0091b833f3e2cebe831a9359e1818e563
thelively32-hub <thelively32@gmail.com>
2026-04-16 02:17:20 -0500

    Refactor database connection string handling

--- db/index.js
+++ db/index.js
@@ -1,7 +1,11 @@
 const { Pool } = require('pg');
 
+const connectionString = process.env.DATABASE_URL || process.env.POSTGRES_URL;
+
+console.log('DB connecting to:', connectionString ? connectionString.split('@')[1] : 'NO URL FOUND');
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
   ssl: false,
 });
 
