c7edcfd3ea5d2fd907afafca440d57b2c9f47afa
thelively32-hub <thelively32@gmail.com>
2026-04-16 00:31:37 -0500

    Add numerology functions for calculations

---
+++ numerology/index.js
@@ -0,0 +1,30 @@
+function reduce(n) {
+  while (n > 9) {
+    n = String(n).split('').reduce((a, d) => a + parseInt(d), 0);
+  }
+  return n;
+}
+
+function destinyNumber(birthDate) {
+  if (!birthDate) return 1;
+  const [y, m, d] = String(birthDate).split('T')[0].split('-').map(Number);
+  if (!y || !m || !d) return 1;
+  return reduce(y + m + d);
+}
+
+function vibrationDay(date) {
+  if (!date) return 1;
+  const [y, m, d] = String(date).split('T')[0].split('-').map(Number);
+  if (!y) return 1;
+  return reduce(y + m + d);
+}
+
+function vibrationPerson(birthDate, targetDate) {
+  if (!birthDate || !targetDate) return 1;
+  const [by, bm, bd] = String(birthDate).split('T')[0].split('-').map(Number);
+  const [ty, tm, td] = String(targetDate).split('T')[0].split('-').map(Number);
+  if (!by || !ty) return 1;
+  return reduce(by + bm + bd + ty + tm + td);
+}
+
+module.exports = { reduce, destinyNumber, vibrationDay, vibrationPerson };
